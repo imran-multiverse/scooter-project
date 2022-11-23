2f1e0ffd52ecf82916d135da166095e54fcfb94a
imran-multiverse <imran.bashir@multiverse.io>
2022-11-23 16:37:47 +0000

    added properties to user class

--- src/User.js
+++ src/User.js
@@ -1,5 +1,12 @@
 class User {
-  // User code here
+  //instance
+  constructor (username, password, age) {
+
+  this.username = username;
+  this.password = password;
+  this.age = age;
+}
+
 }
 
 module.exports = User
--- tests/User.test.js
+++ tests/User.test.js
@@ -1,9 +1,21 @@
 const User = require('../src/User')
 
-// User tests here
+describe('The user class',() => {
 
-// test username
+    const user1 = new User("imran.bashir@multiverse.io","password1",37)
+    const user2 = new User("rayaan.bashir@multivers.io","password2",5)
 
-// test password
+    test('has a username', () => {
+        expect(user1.username).toBe('imran.bashir@multiverse.io')
+    })
 
-// test age
+    test('has a password', () => {
+        expect(user1.password).toBe('password1')
+
+    })
+
+    test('has a age', () => {
+        expect(user1.age).toBe(37)
+
+    })
+})
