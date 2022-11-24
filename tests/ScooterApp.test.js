c567c9753b4f2849e2655687db2245d25770bcbd
imran-multiverse <imran.bashir@multiverse.io>
2022-11-24 17:26:20 +0000

    validation for age & existing user registration now in place

--- tests/ScooterApp.test.js
+++ tests/ScooterApp.test.js
@@ -3,9 +3,15 @@ const User = require('../src/User')
 const ScooterApp = require('../src/ScooterApp')
 
 // ScooterApp tests here
+const TheNewUser1 = new User("imran.bashir@multiverse.io","password1",54)
+const TheNewUser2 = new User("rayaan.bashir@multiverse.io","password1",37)
 
+const scooterApp = new ScooterApp()
 // register user
+scooterApp.register(TheNewUser1);
+scooterApp.register(TheNewUser2);
 
+console.log(ScooterApp,'\n');
 // log in
 
 // add scooter
